38e30e6cf4e160f79d893c623e7dec65e2d9a6ae
HieuThao <you@example.com>
2019-07-15 23:37:17 +0700

    bt

---
+++ src/main/java/com/codegym/SecurityWebApplicationIntializer.java
@@ -0,0 +1,6 @@
+package com.codegym;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationIntializer extends AbstractSecurityWebApplicationInitializer {
+}
